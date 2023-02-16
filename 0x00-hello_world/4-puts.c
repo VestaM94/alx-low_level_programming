92650535b6e0cf85013e636e2609b2b03ba39f4c
VestaM94 <manganye.so@gmail.com>
2023-02-16 11:25:31 -0800

    betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
-#include <stdio.h>
-
-/*
- * function starts
+/**
+ * main function  prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
