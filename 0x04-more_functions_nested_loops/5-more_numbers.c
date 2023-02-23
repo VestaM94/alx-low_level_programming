27745eb35746729a2fd944d0b80d0d8d1bf27c18
VestaM94 <manganye.so@gmail.com>
2023-02-23 15:02:15 -0800

    fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,14 +6,30 @@
 
 void more_numbers(void)
 {
-int i;
-int x;
-for (x = 0; x < 10; x++)
-{
-for (i = 0; i <= 14; i++)
-{
-if (i > 9)
-_putchar((i / 10) + '0');
-_putchar((i % 10) + '0');
+	int line, n, repeat, limit;
+
+	n = '0';
+	repeat = 0;
+	limit = '9';
+
+	for (line = 0; line < 10; line++)
+	{
+		while (repeat < 2)
+		{
+			while (n <= limit)
+			{
+				if (limit == '4')
+					_putchar('1');
+				_putchar(n);
+				n++;
+			}
+			repeat++;
+			limit = '4';
+			n = '0';
+		}
+		_putchar('\n');
+		repeat = 0;
+		n = '0';
+		limit = '9';
+	}
 }
-_putchar('\n');
